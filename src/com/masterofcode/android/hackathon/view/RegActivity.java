959a0c90afc3ab958df3904c40a0e0f1f2b84069
Evgeniy <evgeniy@EVGENIY-PC.(none)>
2011-12-28 21:51:44 +0200

    3

--- src/com/masterofcode/android/hackathon/view/RegActivity.java
+++ src/com/masterofcode/android/hackathon/view/RegActivity.java
@@ -1,7 +1,10 @@
 package com.masterofcode.android.hackathon.view;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class RegActivity extends Activity {
     /** Called when the activity is first created. */
@@ -9,5 +12,17 @@ public class RegActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.signin);
+        
+        
+        Button btn = (Button) findViewById(R.id.btn_name);
+        btn.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent(RegActivity.this,CameraActivity.class);
+				startActivity(intent);
+				
+			}
+		});
     }
 }
